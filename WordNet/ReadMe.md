e469d984986baa4c181908eb12497c6cc610d1cd
Jingwen Gong <gongjing@usc.edu>
2018-10-15 13:43:30 -0700

    Add wordnet to get synonym

---
+++ WordNet/ReadMe.md
@@ -0,0 +1,9 @@
+
+Reference: https://www.geeksforgeeks.org/get-synonymsantonyms-nltk-wordnet-python/
+
+How to install NLTK? 
+	sudo pip install -U nltk
+	sudo pip install -U numpy
+	run python then type import nltk
+	run nltk.download() to get the corpus
+reference: http://www.nltk.org/install.html
---
+++ WordNet/getSynonym.py
@@ -0,0 +1,18 @@
+import nltk 
+from nltk.corpus import wordnet 
+synonyms = [] 
+antonyms = [] 
+
+currWord = "hit"
+def getSynonym ( word ):
+	for syn in wordnet.synsets(word): 
+	    for l in syn.lemmas(): 
+	        synonyms.append(l.name()) 
+	        if l.antonyms(): 
+	            antonyms.append(l.antonyms()[0].name())
+
+	# print(set(synonyms)) 
+	return set(synonyms)
+
+synonyms = getSynonym(currWord)
+print(synonyms)
